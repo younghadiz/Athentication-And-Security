5dd778e49b92ebb5d3b09f484009abe9df0b9118
Younghadiz <gafarioladele1@gmail.com>
2022-05-14 17:05:26 +0100

    Added Hashing To User Passwords

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
-console.log(process.env.API_KEY);
-  
+// console.log(md5("123456"));
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -27,9 +27,6 @@ const userSchema = new mongoose.Schema({
 //model for user schema
 const User = new mongoose.model("User", userSchema);
 
-//the line of code below served as our encryption code for 'password' or any field we want
-
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"]});
 
 
 app.get("/", function(req, res){
@@ -48,7 +45,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
 	const newUser = new User({
 		email: req.body.username,
-		password: req.body.password
+		password: md5(req.body.password)
 	});
 
 	newUser.save(function(err){
@@ -64,7 +61,7 @@ app.post("/register", function(req, res){
 //login
 app.post("/login", function(req, res){
 	const username = req.body.username;
-	const password = req.body.password;
+	const password = md5(req.body.password);
 
 	//now check with the database
 	User.findOne({email:username}, function(err, foundUser){
